59316e04e3bc4a29c4505d3dd2989306f0a06eb1
jashu <jashur@gmail.com>
2025-01-21 10:02:53 +0000

    databse

--- database.sh
+++ database.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/expense-log"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FOLDER="/var/log/expense-log"
+LOG_FILE=$( echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%D-%H-%M-%S)
+LOG_FILE_NAME=$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log
 
-VALIDATE(){
+VALIDATE()
+{
     if [ $1 -ne 0 ]
     then
         echo -e "$2 ... $R FAILURE $N"
@@ -24,31 +23,30 @@ VALIDATE(){
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
+        echo " error:: need sudo acces to excute $R "
+        exit 1
     fi
 }
 
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
-
+echo "scrpit started excuting at : $TIMESTAMP " &>>$LOG_FILE_NAME
 CHECK_ROOT
 
 dnf install mysql-server -y &>>$LOG_FILE_NAME
-VALIDATE $? "Installing MySQL Server"
+VALIDATE $? "Installing mysql server $Y "
 
 systemctl enable mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "Enabling MySQL Server"
+VALIDATE $? "Enabling mysql $Y "
 
 systemctl start mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "Starting MySQL Server"
+VALIDATE $? "Starting mysql $Y "
 
-mysql -h mysql.aws82s.shop -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
+mysql -h mysql.aws82s.shop -u root -pExpenseApp@1 -e 'show database';
 
 if [ $? -ne 0 ]
-then
-    echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
-    mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "Setting Root Password"
+then 
+    echo " root password not setup " &>>$LOG_FILE_NAME
+    mysql_secure_installation  --set-root-pass ExpenseApp@1
+    VALIDATE $? "setting root password"
 else
-    echo -e "MySQL Root password already setup ... $Y SKIPPING $N"
+    echo "root password already setup...  $Y skipping $N "
 fi
