58665b7855925849af6be40552785c1980581eb0
jashu <jashur@gmail.com>
2025-01-21 11:26:19 +0000

    backend

---
+++ backend.sh
@@ -0,0 +1,78 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-log"
+LOG_FILE=$( echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo " error:: need sudo acces to excute $R "
+        exit 1
+    fi
+} 
+
+echo "scrpit started excuting at : $TIMESTAMP " &>>$LOG_FILE_NAME
+CHECK_ROOT
+
+dnf module disable nodejs -y $LOG_FILE_NAME
+VALIDATE $? "disabling existing nodejs"
+
+dnf module enable nodejs:20 -y $LOG_FILE_NAME
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y $LOG_FILE_NAME
+VALIDATE $? "installing nodejs"
+
+useradd expense $LOG_FILE_NAME
+VALIDATE $? "adding expense user"
+
+mkdir -p /app $LOG_FILE_NAME
+VALIDATE $? "creating app directoty"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
+
+cd /app
+rm -rf /app
+
+unzip/tmp/backend.zip $LOG_FILE_NAME
+VALIDATE $? "UNZIPPING backendfile"
+
+npm install 
+VALIDATE $? "installing node package manager"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# Prepare MySQL Schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.aws82s.shop -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
